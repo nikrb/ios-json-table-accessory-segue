5422593aae1c9f0fd25bf20254f2d3ad156f9f08
nikrb <nik_rb@yahoo.com>
2016-03-01 04:57:10 +0000

    Initial Commit

---
+++ Cjson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cjson
+//
+//  Created by Nick Scott on 01/03/2016.
+//  Copyright © 2016 Nick Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
